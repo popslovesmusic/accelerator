27ba3fb39c137defb90af6fbe01f182fcc5c0f14
popslovesmusic <popslovesmusic@users.noreply.github.com>
2026-05-02 03:19:50 -0400

    Remove oneproc from fork

--- oneproc/commands/claim.py
+++
@@ -1,6 +0,0 @@
-import typer
-app = typer.Typer()
-@app.callback(invoke_without_command=True)
-def main():
-    """Show/audit/promote/downgrade claims."""
-    typer.echo("Claim command not yet implemented.")
